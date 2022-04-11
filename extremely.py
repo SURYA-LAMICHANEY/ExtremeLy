7971eee93672ba0e6417f9dcc4164be01ae23e11
Surya Lamichaney <66554438+SURYA-LAMICHANEY@users.noreply.github.com>
2022-04-11 10:49:54 +0530

    Update extremely.py

--- extremely.py
+++ extremely.py
@@ -1,22 +1,3 @@
-###########################################################################################################
-"""
-ExtremeLy is a python package for Extreme Value Analysis. It was found that there are not many packages
-for EVA in python. Among existing packages some of them were incomplete, some of them were internally using R 
-packages and some had only basic implementations without any plots for model assessment. So ExtremeLy brings all 
-those packages together, removes R dependencies and provides most of the fucntionalities for EVA in python
-without being dependent on R packages. Some fucntionalities from the already existing packages have been used
-as they are, some have been modified to accomodate additional requirements and for some just the R dependencies
-are replaced with python implementation. The three already existing packages that are used here are:
-    1. scikit-extremes (skextremes) - https://scikit-extremes.readthedocs.io/en/latest/
-    2. thresholdmodeling - https://github.com/iagolemos1/thresholdmodeling
-    3. evt - https://pypi.org/project/evt/#description
-"""
-############################################################################################################
-"""
-There are basically two approaches to Extreme Value Analysis:
-    1. Block maxima method + Generalized Extreme Value (GEV) Distribution.
-    2. Peak-over-threshold method + Generalized Pareto Distribution (GPD).
-"""
 #importing all the required libraries
 import pandas as pd
 import numpy as np
